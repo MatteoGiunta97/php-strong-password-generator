8fd8bd9f926a3dadd1196941838045e84058dadc
MatteoGiunta97 <matteogiunta@outlook.it>
2024-05-03 17:02:49 +0200

    feat: add length input

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
 Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
 */
+
+$pwLength = $_GET['pwLength'];
 ?>
 
 <!DOCTYPE html>
@@ -25,8 +27,21 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PW Gen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
+    <div class="container">
+        <form method="get">
+            <div class="mb-3">
+                <label for="pw-length" class="form-label">Password Length:</label>
+                <input name="pwLength" id="pw-length" type="text" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <?php var_dump($pwLength); ?>
+        </form>
+    </div>    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
